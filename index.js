6a5cc1ab555eef0c89228d8acb1deb2cc41fc5ee
Nikita Dedukh <Deduh@email.com>
2023-01-02 14:49:56 +0600

    my first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+ function isPalindrome(x)
+ {
+     let arr1 = Array.from(x.toString())
+     let arr2 = Array.from(x.toString()).reverse()
+     for (let i = 0; i < arr1.length - 1; i++)
+     {
+         if (arr1[i] !== arr2[i]) return false
+     }
+     return true
+ }
+ console.log(isPalindrome(121))
+ console.log(isPalindrome(123))
+ console.log(isPalindrome(123321))
+ console.log(isPalindrome(-123321))
